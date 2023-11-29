95e47ff5dea4a7b4cce991ebe2548c01bc3ace55
Joan Macharia <joan.macharia@students.moringa>
2023-11-29 09:30:30 +0300

    Completed lab

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,11 @@
 from user import User
 
 class Student(User):
+
+    def __init__(self, first_name, last_name, knowledge=[]) -> None:
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge
     
-    def learn(self):
+    def learn(self, new_information):
+        return self.knowledge.append(new_information)
         pass
